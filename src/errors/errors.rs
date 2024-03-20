83c4998b1e230333c02e26c966e0cde08009159c
am-cid <mcidantonio@gmail.com>
2024-03-20 21:20:30 +0800

    feat: unknown token error init

--- src/errors/errors.rs
+++ src/errors/errors.rs
@@ -63,3 +63,35 @@ impl CompilerError for DelimError {
         msg
     }
 }
+
+pub struct UnknownTokenError {
+    line_text: &'static str,
+    pos: (usize, usize),
+}
+
+impl UnknownTokenError {
+    pub fn new(line_text: &'static str, pos: (usize, usize)) -> Self {
+        Self { line_text, pos }
+    }
+}
+
+impl CompilerError for UnknownTokenError {
+    fn message(&self) -> String {
+        let mut msg: String = "".to_string();
+        msg.push_str(
+            format!(
+                "[UNKNOWN TOKEN] at line {}, col {}\n",
+                self.pos.0, self.pos.1
+            )
+            .as_str(),
+        );
+        let line_length = self.line_text.len();
+        let border = "-".repeat(line_length);
+        let highlight = " ".repeat(self.pos.1) + "^";
+        msg.push_str(format!("------{}\n", border).as_str());
+        msg.push_str(format!("{:width$} | {}\n", self.pos.0, self.line_text, width = 3).as_str());
+        msg.push_str(format!("    | {}\n", highlight).as_str());
+        msg.push_str(format!("------{}\n", border).as_str());
+        msg
+    }
+}
