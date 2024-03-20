878509ccbb66c4d0d867b76acff7444838ebbf44
am-cid <mcidantonio@gmail.com>
2024-03-20 14:21:28 +0800

    rename some methods to describe their actions better

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -43,94 +43,104 @@ impl Lexer {
         while self.pos < self.source.len() {
             match self.curr_char {
                 // TODO: remove unwrap or else:
-                //       - do peek_ident instead if starting with a letter
                 //       - do unknown token if anything else
-                ' ' | '\t' | '\n' => self.peek(TokenType::Whitespace).unwrap_or_else(|_| ()),
-                'b' => self.peek(TokenType::Bweak).unwrap_or(self.peek_ident()),
+                ' ' | '\t' | '\n' => self
+                    .peek_reserved(TokenType::Whitespace)
+                    .unwrap_or_else(|_| ()),
+                'b' => self
+                    .peek_reserved(TokenType::Bweak)
+                    .unwrap_or(self.peek_ident()),
                 'c' => self
-                    .peek(TokenType::Chan)
-                    .or_else(|_| self.peek(TokenType::Cap))
-                    .or_else(|_| self.peek(TokenType::Cwass))
+                    .peek_reserved(TokenType::Chan)
+                    .or_else(|_| self.peek_reserved(TokenType::Cap))
+                    .or_else(|_| self.peek_reserved(TokenType::Cwass))
                     .unwrap_or_else(|_| self.peek_ident()),
                 'd' => self
-                    .peek(TokenType::Dono)
-                    .or_else(|_| self.peek(TokenType::DoWhiwe))
+                    .peek_reserved(TokenType::Dono)
+                    .or_else(|_| self.peek_reserved(TokenType::DoWhiwe))
                     .unwrap_or_else(|_| self.peek_ident()),
                 'e' => self
-                    .peek(TokenType::EwseIwf)
-                    .or_else(|_| self.peek(TokenType::Ewse))
+                    .peek_reserved(TokenType::EwseIwf)
+                    .or_else(|_| self.peek_reserved(TokenType::Ewse))
                     .unwrap_or_else(|_| self.peek_ident()),
                 'f' => self
-                    .peek(TokenType::Fow)
-                    .or_else(|_| self.peek(TokenType::Fax))
-                    .or_else(|_| self.peek(TokenType::Fwunc))
+                    .peek_reserved(TokenType::Fow)
+                    .or_else(|_| self.peek_reserved(TokenType::Fax))
+                    .or_else(|_| self.peek_reserved(TokenType::Fwunc))
                     .unwrap_or_else(|_| self.peek_ident()),
-                'g' => self.peek(TokenType::Gwobaw).unwrap_or(self.peek_ident()),
+                'g' => self
+                    .peek_reserved(TokenType::Gwobaw)
+                    .unwrap_or(self.peek_ident()),
                 'i' => self
-                    .peek(TokenType::Iwf)
-                    .or_else(|_| self.peek(TokenType::Inpwt))
+                    .peek_reserved(TokenType::Iwf)
+                    .or_else(|_| self.peek_reserved(TokenType::Inpwt))
                     .unwrap_or_else(|_| self.peek_ident()),
                 'm' => self
-                    .peek(TokenType::Mainuwu)
+                    .peek_reserved(TokenType::Mainuwu)
                     .unwrap_or_else(|_| self.peek_ident()),
                 'n' => self
-                    .peek(TokenType::Nuww)
+                    .peek_reserved(TokenType::Nuww)
                     .unwrap_or_else(|_| self.peek_ident()),
                 'p' => self
-                    .peek(TokenType::Pwint)
+                    .peek_reserved(TokenType::Pwint)
                     .unwrap_or_else(|_| self.peek_ident()),
                 's' => self
-                    .peek(TokenType::San)
-                    .or_else(|_| self.peek(TokenType::Senpai))
-                    .or_else(|_| self.peek(TokenType::Sama))
+                    .peek_reserved(TokenType::San)
+                    .or_else(|_| self.peek_reserved(TokenType::Senpai))
+                    .or_else(|_| self.peek_reserved(TokenType::Sama))
                     .unwrap_or_else(|_| self.peek_ident()),
                 'w' => self
-                    .peek(TokenType::Whiwe)
-                    .or_else(|_| self.peek(TokenType::Wetuwn))
+                    .peek_reserved(TokenType::Whiwe)
+                    .or_else(|_| self.peek_reserved(TokenType::Wetuwn))
                     .unwrap_or_else(|_| self.peek_ident()),
                 'a'..='z' | 'A'..='Z' => self.peek_ident(),
                 '0'..='9' => self.peek_num(),
                 // TODO: peek string part mid/end instead of unit for pipe |
-                '|' => self.peek(TokenType::Or).unwrap_or_else(|_| ()),
-                '&' => self.peek(TokenType::And).unwrap_or_else(|_| ()),
+                '|' => self.peek_reserved(TokenType::Or).unwrap_or_else(|_| ()),
+                '&' => self.peek_reserved(TokenType::And).unwrap_or_else(|_| ()),
                 '=' => self
-                    .peek(TokenType::Equal)
-                    .or_else(|_| self.peek(TokenType::Assign))
+                    .peek_reserved(TokenType::Equal)
+                    .or_else(|_| self.peek_reserved(TokenType::Assign))
                     .unwrap_or_else(|_| ()),
                 '-' => self
-                    .peek(TokenType::Decrement)
-                    .or_else(|_| self.peek(TokenType::Dash))
+                    .peek_reserved(TokenType::Decrement)
+                    .or_else(|_| self.peek_reserved(TokenType::Dash))
                     .unwrap_or_else(|_| ()),
-                // TODO: add unknown token error when cannot be unwraped
-                '!' => self.peek(TokenType::NotEqual).unwrap_or_else(|_| ()),
+                '!' => self
+                    .peek_reserved(TokenType::NotEqual)
+                    .unwrap_or_else(|_| ()),
                 '<' => self
-                    .peek(TokenType::LessEqual)
-                    .or_else(|_| self.peek(TokenType::LessThan))
+                    .peek_reserved(TokenType::LessEqual)
+                    .or_else(|_| self.peek_reserved(TokenType::LessThan))
                     .unwrap_or_else(|_| ()),
                 '>' => self
                     .peek_multi_line_comment()
                     .or_else(|_| self.peek_single_line_comment())
-                    .or_else(|_| self.peek(TokenType::GreaterEqual))
-                    .or_else(|_| self.peek(TokenType::GreaterThan))
+                    .or_else(|_| self.peek_reserved(TokenType::GreaterEqual))
+                    .or_else(|_| self.peek_reserved(TokenType::GreaterThan))
                     .unwrap_or_else(|_| ()),
-                '*' => self.peek(TokenType::Multiply).unwrap_or_else(|_| ()),
-                '/' => self.peek(TokenType::Divide).unwrap_or_else(|_| ()),
-                '%' => self.peek(TokenType::Modulo).unwrap_or_else(|_| ()),
-                '{' => self.peek(TokenType::LBrace).unwrap_or_else(|_| ()),
-                '}' => self.peek(TokenType::RBrace).unwrap_or_else(|_| ()),
-                '(' => self.peek(TokenType::LParen).unwrap_or_else(|_| ()),
-                ')' => self.peek(TokenType::RParen).unwrap_or_else(|_| ()),
+                '*' => self
+                    .peek_reserved(TokenType::Multiply)
+                    .unwrap_or_else(|_| ()),
+                '/' => self.peek_reserved(TokenType::Divide).unwrap_or_else(|_| ()),
+                '%' => self.peek_reserved(TokenType::Modulo).unwrap_or_else(|_| ()),
+                '{' => self.peek_reserved(TokenType::LBrace).unwrap_or_else(|_| ()),
+                '}' => self.peek_reserved(TokenType::RBrace).unwrap_or_else(|_| ()),
+                '(' => self.peek_reserved(TokenType::LParen).unwrap_or_else(|_| ()),
+                ')' => self.peek_reserved(TokenType::RParen).unwrap_or_else(|_| ()),
                 '[' => self
-                    .peek(TokenType::DoubleLBracket)
-                    .or_else(|_| self.peek(TokenType::LBracket))
+                    .peek_reserved(TokenType::DoubleLBracket)
+                    .or_else(|_| self.peek_reserved(TokenType::LBracket))
                     .unwrap_or_else(|_| ()),
                 ']' => self
-                    .peek(TokenType::DoubleRBracket)
-                    .or_else(|_| self.peek(TokenType::RBracket))
+                    .peek_reserved(TokenType::DoubleRBracket)
+                    .or_else(|_| self.peek_reserved(TokenType::RBracket))
                     .unwrap_or_else(|_| ()),
-                ',' => self.peek(TokenType::Comma).unwrap_or_else(|_| ()),
-                '.' => self.peek(TokenType::Dot).unwrap_or_else(|_| ()),
-                '~' => self.peek(TokenType::Terminator).unwrap_or_else(|_| ()),
+                ',' => self.peek_reserved(TokenType::Comma).unwrap_or_else(|_| ()),
+                '.' => self.peek_reserved(TokenType::Dot).unwrap_or_else(|_| ()),
+                '~' => self
+                    .peek_reserved(TokenType::Terminator)
+                    .unwrap_or_else(|_| ()),
                 // TODO: remove default case
                 _ => break,
             }
@@ -152,7 +162,7 @@ impl Lexer {
             self.peek_char = self.source.chars().nth(self.pos + 1).unwrap_or('\n');
         }
     }
-    fn expect_peek(&mut self, expected: char) -> bool {
+    fn expect_peek_char_is(&mut self, expected: char) -> bool {
         match expected == self.peek_char {
             true => {
                 self.advance(1);
@@ -184,7 +194,7 @@ impl Lexer {
             self.curr_char = self.source.chars().nth(self.pos).unwrap_or('\n')
         }
     }
-    pub fn peek(&mut self, expected: TokenType) -> Result<(), ()> {
+    fn peek_reserved(&mut self, expected: TokenType) -> Result<(), ()> {
         let expect_str = expected.to_str();
         for i in 0..expect_str.len() - 1 {
             if self.curr_char != expect_str.chars().nth(i).unwrap() {
@@ -238,10 +248,10 @@ impl Lexer {
     }
     fn peek_single_line_comment(&mut self) -> Result<(), ()> {
         let mut tmp: String = "".to_string();
-        if !self.expect_peek('.') {
+        if !self.expect_peek_char_is('.') {
             return Err(());
         }
-        if !self.expect_peek('<') {
+        if !self.expect_peek_char_is('<') {
             self.reverse(1); // TODO: add error here, not reverse
             return Err(());
         }
@@ -267,14 +277,14 @@ impl Lexer {
     fn peek_multi_line_comment(&mut self) -> Result<(), ()> {
         let mut tmp: String = "".to_string();
         let start_pos = (self.d_pos.0, self.d_pos.1);
-        if !self.expect_peek('/') {
+        if !self.expect_peek_char_is('/') {
             return Err(());
         }
-        if !self.expect_peek('/') {
+        if !self.expect_peek_char_is('/') {
             self.reverse(1);
             return Err(());
         }
-        if !self.expect_peek('<') {
+        if !self.expect_peek_char_is('<') {
             self.reverse(2);
             return Err(());
         }
@@ -290,16 +300,16 @@ impl Lexer {
             }
             // has closing >//<
             if self.curr_char == '>' {
-                if !self.expect_peek('/') {
+                if !self.expect_peek_char_is('/') {
                     tmp.push('>');
                     continue;
                 }
-                if !self.expect_peek('/') {
+                if !self.expect_peek_char_is('/') {
                     println!("{}", self.peek_char);
                     tmp.push_str(">/");
                     continue;
                 }
-                if !self.expect_peek('<') {
+                if !self.expect_peek_char_is('<') {
                     tmp.push_str(">//");
                     continue;
                 }
