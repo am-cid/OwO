1c237f26a0a8f58e151bcd436b010e5c2434d613
am-cid <mcidantonio@gmail.com>
2024-03-22 01:00:47 +0800

    feat: help arg parsing and execution

--- src/cli/help.rs
+++ src/cli/help.rs
@@ -9,9 +9,25 @@ impl Command for HelpCommand {
         todo!()
     }
     fn parse(&self) -> Result<(), String> {
-        todo!()
+        match self.arg.as_str() {
+            "" | "help" | "lex" | "parse" | "analyze" | "compile" | "run" | "version" => Ok(()),
+            _ => Err(format!(
+                "'{}' is not a valid arg for 'help'. See 'help help' for more information",
+                self.arg
+            )),
+        }
     }
     fn exec(&self) -> Result<(), String> {
-        todo!()
+        match self.arg.as_str() {
+            "" | "help" => Self::help_msg(true),
+            "lex" => lex::LexCommand::help_msg(true),
+            "parse" => parse::ParseCommand::help_msg(true),
+            "analyze" => analyze::AnalyzeCommand::help_msg(true),
+            "compile" => compile::CompileCommand::help_msg(true),
+            "run" => run::RunCommand::help_msg(true),
+            "version" => version::VersionCommand::help_msg(true),
+            _ => unreachable!(),
+        };
+        Ok(())
     }
 }
