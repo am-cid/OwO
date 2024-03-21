e99323f369837df8e1d76380a09208f3ada0608e
am-cid <mcidantonio@gmail.com>
2024-03-22 01:00:27 +0800

    fix: `help_msg` does not need reference to `self`

--- src/cli/compile.rs
+++ src/cli/compile.rs
@@ -1,11 +1,11 @@
-use crate::cli::command::Command;
+use crate::cli::commands::Command;
 
 pub struct CompileCommand {
     pub arg: String,
     pub flags: Option<Vec<String>>,
 }
 impl Command for CompileCommand {
-    fn help_msg(&self, verbose: bool) -> String {
+    fn help_msg(verbose: bool) -> String {
         todo!()
     }
     fn parse(&self) -> Result<(), String> {
