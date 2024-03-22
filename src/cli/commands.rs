7e11abf9c8359a475ed865ba940967930d7f6a1a
am-cid <mcidantonio@gmail.com>
2024-03-22 13:52:51 +0800

    refactor: use Box dyn Trait instead of enums in tokenizing os args
    this is to get rid of boilerplate

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -3,64 +3,37 @@ use crate::cli::{
     parse::ParseCommand, run::RunCommand, version::VersionCommand,
 };
 
-pub trait CommandType {
-    fn help_msg(verbose: bool);
+pub trait Command {
+    fn help_msg(verbose: bool)
+    where
+        Self: Sized;
     fn parse(&self) -> Result<(), String>;
     fn exec(&self) -> Result<(), String>;
 }
 // Put commands here
-pub enum Command {
-    Help(HelpCommand),
-    Version(VersionCommand),
-    Lex(LexCommand),
-    Parse(ParseCommand),
-    Analyze(AnalyzeCommand),
-    Compile(CompileCommand),
-    Run(RunCommand),
+pub fn to_command(
+    name: String,
+    arg: String,
+    flags: Option<Vec<String>>,
+) -> Result<Box<dyn Command>, String> {
+    match name.as_str() {
+        "help" | "" => Ok(Box::new(HelpCommand { arg, flags })),
+        "version" => Ok(Box::new(VersionCommand { arg, flags })),
+        "lex" => Ok(Box::new(LexCommand { arg, flags })),
+        "parse" => Ok(Box::new(ParseCommand { arg, flags })),
+        "analyze" => Ok(Box::new(AnalyzeCommand { arg, flags })),
+        "compile" => Ok(Box::new(CompileCommand { arg, flags })),
+        "run" => Ok(Box::new(RunCommand { arg, flags })),
+        _ => Err(format!("Unknown command {}", name)),
+    }
 }
-impl Command {
-    pub fn tokenize(args: Vec<String>) -> Result<Command, String> {
-        let mut args_iter = args.into_iter();
-        let command = args_iter.next().unwrap_or("".to_string());
-        let arg = args_iter.next().unwrap_or("".to_string());
-        let flags = match args_iter.len() {
-            0 => None,
-            _ => Some(args_iter.collect()),
-        };
-        Command::new(command, arg, flags)
-    }
-    fn new(name: String, arg: String, flags: Option<Vec<String>>) -> Result<Self, String> {
-        match name.as_str() {
-            "help" | "" => Ok(Self::Help(HelpCommand { arg, flags })),
-            "version" => Ok(Self::Version(VersionCommand { arg, flags })),
-            "lex" => Ok(Self::Lex(LexCommand { arg, flags })),
-            "parse" => Ok(Self::Parse(ParseCommand { arg, flags })),
-            "analyze" => Ok(Self::Analyze(AnalyzeCommand { arg, flags })),
-            "compile" => Ok(Self::Compile(CompileCommand { arg, flags })),
-            "run" => Ok(Self::Run(RunCommand { arg, flags })),
-            _ => Err(format!("Unknown command {}", name)),
-        }
-    }
-    pub fn parse(&self) -> Result<(), String> {
-        match self {
-            Self::Help(cmd) => cmd.parse(),
-            Self::Version(cmd) => cmd.parse(),
-            Self::Lex(cmd) => cmd.parse(),
-            Self::Parse(cmd) => cmd.parse(),
-            Self::Analyze(cmd) => cmd.parse(),
-            Self::Compile(cmd) => cmd.parse(),
-            Self::Run(cmd) => cmd.parse(),
-        }
-    }
-    pub fn exec(&self) -> Result<(), String> {
-        match self {
-            Self::Help(cmd) => cmd.exec(),
-            Self::Version(cmd) => cmd.exec(),
-            Self::Lex(cmd) => cmd.exec(),
-            Self::Parse(cmd) => cmd.exec(),
-            Self::Analyze(cmd) => cmd.exec(),
-            Self::Compile(cmd) => cmd.exec(),
-            Self::Run(cmd) => cmd.exec(),
-        }
-    }
+pub fn tokenize(args: Vec<String>) -> Result<Box<dyn Command>, String> {
+    let mut args_iter = args.into_iter();
+    let command = args_iter.next().unwrap_or("".to_string());
+    let arg = args_iter.next().unwrap_or("".to_string());
+    let flags = match args_iter.len() {
+        0 => None,
+        _ => Some(args_iter.collect()),
+    };
+    to_command(command, arg, flags)
 }
