f93231f5df9434cb0c8e5978d149e333670dcca5
am-cid <mcidantonio@gmail.com>
2024-03-22 11:15:15 +0800

    feat: appease the bc by using enum return type instead of impl trait
    i thought you could return any type that implements a trait like go does. my bad

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ mod cli;
 mod errors;
 mod lexer;
 
-use cli::commands::{tokenize, Command};
+use cli::commands::Command;
 use std::env;
 
 fn main() {
-    let os_args: Vec<String> = env::args().collect();
-    tokenize(os_args)
+    let os_args: Vec<String> = env::args().skip(1).collect();
+    Command::tokenize(os_args)
         .and_then(|cmd| cmd.parse().map(move |_| cmd))
         .and_then(|cmd| cmd.exec())
         .unwrap_or_else(|e| println!("{}", e));
