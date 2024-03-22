95d8670fb0eca1e08919127d10dbbfb7e92a7284
am-cid <mcidantonio@gmail.com>
2024-03-22 23:14:59 +0800

    feat: complete initial help messages for `compile` and `run`

--- src/cli/compile.rs
+++ src/cli/compile.rs
@@ -1,4 +1,9 @@
-use crate::cli::commands::Command;
+use std::path::Path;
+
+use crate::{
+    cli::commands::Command,
+    utils::{path::PathExt, string::StringExt},
+};
 
 pub struct Compile {
     pub arg: String,
@@ -6,10 +11,35 @@ pub struct Compile {
 }
 impl Command for Compile {
     fn help_msg(verbose: bool) {
-        todo!()
+        let mut title = "compile".to_string().pad_right(16).fill_left(2).bold();
+        if verbose {
+            title = title.underline();
+        }
+        println!(
+            "{}{}\n{}\n",
+            title,
+            "Compiles a selected source file. Outputs an executable",
+            "file in the same directory as the inputted source file"
+                .to_string()
+                .fill_left(18),
+        );
+        if verbose {
+            println!(
+                "\n{}{}",
+                "Usage:".to_string().bold().underline().fill_left(2),
+                "owo compile path/to/source.uwu".to_string().fill_left(10),
+            );
+        }
     }
     fn parse(&self) -> Result<(), String> {
-        todo!()
+        Path::new(&self.arg)
+            .canonicalize()
+            .map_err(|_| format!("Failed to canonicalize path: '{}'", self.arg))?
+            .must_be_file()?
+            .extension()
+            .map_or(false, |ext| ext == "uwu")
+            .then(|| ())
+            .ok_or(format!("\"{}\" is not a .uwu file", self.arg))
     }
     fn exec(&self) -> Result<(), String> {
         todo!()
