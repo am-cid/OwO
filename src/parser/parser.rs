0efccf4cc958c85324b757aed9b8858787d37286
am-cid <mcidantonio@gmail.com>
2024-03-24 00:01:12 +0800

    feat: register tokens with its respective parsing fn
    parsing fns are placeholders for now

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 use crate::{
     lexer::token::{Token, TokenType},
@@ -89,6 +90,67 @@ impl Parser {
             self.peek_tok = self.tokens[self.pos];
         }
     }
+    fn register_init(&mut self) -> () {
+        self.register_prefix(TokenType::Dash, Self::parse_prefix_expression);
+        self.register_prefix(TokenType::LParen, Self::parse_grouped_expression);
+        self.register_prefix(TokenType::Identifier, Self::parse_ident);
+
+        self.register_prefix_special(TokenType::LParen, Self::parse_grouped_expression);
+        self.register_prefix_special(TokenType::LBrace, Self::parse_array);
+        self.register_prefix_special(TokenType::StringPartStart, Self::parse_gen_string);
+
+        // literals (just returns curr_tok)
+        self.register_prefix(TokenType::IntLiteral, Self::parse_literal);
+        self.register_prefix(TokenType::FloatLiteral, Self::parse_literal);
+        self.register_prefix(TokenType::Fax, Self::parse_literal);
+        self.register_prefix(TokenType::Cap, Self::parse_literal);
+
+        self.register_prefix_special(TokenType::IntLiteral, Self::parse_literal);
+        self.register_prefix_special(TokenType::StringLiteral, Self::parse_gen_string);
+        self.register_prefix_special(TokenType::FloatLiteral, Self::parse_literal);
+        self.register_prefix_special(TokenType::Fax, Self::parse_literal);
+        self.register_prefix_special(TokenType::Cap, Self::parse_literal);
+        self.register_prefix_special(TokenType::Nuw, Self::parse_literal);
+        self.register_prefix_special(TokenType::Inpw, Self::parse_gen_string);
+
+        // infixes
+        self.register_infix(TokenType::Equal, Self::parse_infix_special_expression);
+        self.register_infix(TokenType::NotEqual, Self::parse_infix_special_expression);
+        self.register_infix(TokenType::And, Self::parse_infix_special_expression);
+        self.register_infix(TokenType::Or, Self::parse_infix_special_expression);
+        self.register_infix(TokenType::LessThan, Self::parse_infix_expression);
+        self.register_infix(TokenType::LessEqual, Self::parse_infix_expression);
+        self.register_infix(TokenType::GreaterThan, Self::parse_infix_expression);
+        self.register_infix(TokenType::GreaterEqual, Self::parse_infix_expression);
+
+        self.register_infix_special(TokenType::Equal, Self::parse_infix_special_expression);
+        self.register_infix_special(TokenType::NotEqual, Self::parse_infix_special_expression);
+        self.register_infix_special(TokenType::And, Self::parse_infix_special_expression);
+        self.register_infix_special(TokenType::Or, Self::parse_infix_special_expression);
+
+        // self.register_infix(TokenType::Concat, self.parse_infix_expression)
+        self.register_infix(TokenType::Plus, Self::parse_infix_expression);
+        self.register_infix(TokenType::Dash, Self::parse_infix_expression);
+        self.register_infix(TokenType::Multiply, Self::parse_infix_expression);
+        self.register_infix(TokenType::Divide, Self::parse_infix_expression);
+        self.register_infix(TokenType::Modulo, Self::parse_infix_expression);
+
+        // postfixes
+        self.register_postfix(TokenType::Identifier, Self::parse_postfix_expression);
+        self.register_postfix(TokenType::IntLiteral, Self::parse_postfix_expression);
+        self.register_postfix(TokenType::FloatLiteral, Self::parse_postfix_expression);
+        self.register_postfix(TokenType::RParen, Self::parse_postfix_expression);
+        // self.register_postfix(TokenType::RBrace, Self::parse_postfix_expression);
+
+        // in blocks
+        self.register_in_block(TokenType::Identifier, Self::parse_ident_statement);
+        self.register_in_block(TokenType::Iwf, Self::parse_if_statement);
+        self.register_in_block(TokenType::Wetuwn, Self::parse_return_statement);
+        self.register_in_block(TokenType::Whiwe, Self::parse_while_statement);
+        self.register_in_block(TokenType::DoWhiwe, Self::parse_while_statement);
+        self.register_in_block(TokenType::Fow, Self::parse_for_statement);
+        self.register_in_block(TokenType::Pwint, Self::parse_print);
+    }
 
     // helper fns
     fn curr_tok_is(&self, token_type: TokenType) -> bool {
@@ -104,5 +166,43 @@ impl Parser {
         } else {
             false
         }
+    }
+
+    // register_init helper fns
+    fn register_prefix(&mut self, token_type: TokenType, parser: fn(&mut Self)) -> () {
+        self.prefix_parse_fns.insert(token_type, parser);
+        self.expected_prefix_tokens.push(token_type);
+    }
+    fn register_prefix_special(&mut self, token_type: TokenType, parser: fn(&mut Self)) -> () {
+        self.prefix_special_parse_fns.insert(token_type, parser);
+        self.expected_prefix_special_tokens.push(token_type);
+    }
+    fn register_infix(
+        &mut self,
+        token_type: TokenType,
+        parser: fn(&mut Self, Box<dyn Expression>),
+    ) -> () {
+        self.infix_parse_fns.insert(token_type, parser);
+        self.expected_infix_tokens.push(token_type);
+    }
+    fn register_infix_special(
+        &mut self,
+        token_type: TokenType,
+        parser: fn(&mut Self, Box<dyn Expression>),
+    ) -> () {
+        self.infix_special_parse_fns.insert(token_type, parser);
+        self.expected_infix_special_tokens.push(token_type);
+    }
+    fn register_postfix(
+        &mut self,
+        token_type: TokenType,
+        parser: fn(&mut Self, Box<dyn Expression>),
+    ) -> () {
+        self.postfix_parse_fns.insert(token_type, parser);
+        self.expected_postfix_tokens.push(token_type);
+    }
+    fn register_in_block(&mut self, token_type: TokenType, parser: fn(&mut Self)) -> () {
+        self.in_block_parse_fns.insert(token_type, parser);
+        self.expected_in_block_tokens.push(token_type);
     }
 }
