61e1626b16744b6e4e2a03e1ca64b7a27a5f0e91
am-cid <mcidantonio@gmail.com>
2024-03-23 23:51:13 +0800

    feat: helper macros to not repeat `token()` implementation
    because all productions should be all nodes but only some are statements/expressions, this macro helps not having to write the same implementation for `token()` twice

--- src/parser/productions.rs
+++ src/parser/productions.rs
@@ -10,3 +10,35 @@ pub trait Expression {
     fn token(&self) -> String;
     fn expression(&self);
 }
+// helper macros so we don't have to repeat the same code
+// since token() should be the same for all 3 traits above
+macro_rules! impl_statement {
+    ($T:ident { fn token(&self) -> String { $($body:tt)* } }) => {
+        impl Node for $T {
+            fn token(&self) -> String {
+                stringify!({ $($body)* }).to_string()
+            }
+        }
+        impl Statement for $T {
+            fn token(&self) -> String {
+                stringify!({ $($body)* }).to_string()
+            }
+            fn statement(&self) {}
+        }
+    };
+}
+macro_rules! impl_expression {
+    ($T:ident { fn token(&self) -> String { $($body:tt)* } }) => {
+        impl Node for $T {
+            fn token(&self) -> String {
+                stringify!({ $($body)* }).to_string()
+            }
+        }
+        impl Expression for $T {
+            fn token(&self) -> String {
+                stringify!({ $($body)* }).to_string()
+            }
+            fn expression(&self) {}
+        }
+    };
+}
