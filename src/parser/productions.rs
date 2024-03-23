62a3dd34af519125400b9a48c746e0acd41264de
am-cid <mcidantonio@gmail.com>
2024-03-23 23:48:27 +0800

    feat: node, statement, and expression traits for productions to implement
    this is just to differentiate production structs that will be created later on

--- src/parser/productions.rs
+++ src/parser/productions.rs
@@ -1,1 +1,12 @@
 
+pub trait Node {
+    fn token(&self) -> String;
+}
+pub trait Statement {
+    fn token(&self) -> String;
+    fn statement(&self);
+}
+pub trait Expression {
+    fn token(&self) -> String;
+    fn expression(&self);
+}
