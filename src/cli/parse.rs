ffa04dc9b8998a432b1fc91da58d4c6aa6b85725
am-cid <mcidantonio@gmail.com>
2024-12-02 19:15:45 +0800

    feat(cli): implement parse command

--- src/cli/parse.rs
+++ src/cli/parse.rs
@@ -3,6 +3,7 @@ use std::{fs, path::Path};
 use crate::{
     cli::commands::Command,
     lexer::{lexer::Lexer, token::TokenKind},
+    parser::{parser::Parser, productions::Production},
     utils::{path::PathExt, string::StringExt},
 };
 
@@ -41,6 +42,46 @@ impl Command for Parse {
             .ok_or(format!("\"{}\" is not a .uwu file", self.arg))
     }
     fn exec(&self) -> Result<(), String> {
-        todo!()
+        let abs_path = Path::new(&self.arg)
+            .canonicalize()
+            .expect("This was already validated. Something else went wrong!")
+            .display()
+            .to_string();
+        let trimmed_path = abs_path.strip_prefix(r#"\\?\"#).unwrap_or(&abs_path);
+        // read file
+        let source = Box::leak(
+            fs::read_to_string(trimmed_path)
+                .unwrap_or_default()
+                .into_boxed_str(),
+        );
+        let mut l = Lexer::new(source);
+        l.tokenize();
+        l.pretty_print_tokens();
+        if l.errors.len() > 0 {
+            l.errors.into_iter().for_each(|err| println!("{}", err));
+            return Err(format!("Failed to tokenize file: {}", trimmed_path));
+        }
+        let mut p = Parser::new(
+            source,
+            l.tokens
+                .into_iter()
+                .filter(|tok| match tok.kind {
+                    TokenKind::Whitespace
+                    | TokenKind::Newline
+                    | TokenKind::Tab
+                    | TokenKind::CarriageReturn
+                    | TokenKind::Comment
+                    | TokenKind::EOF => false,
+                    _ => true,
+                })
+                .collect::<Vec<_>>(),
+        );
+        match p.parse_program() {
+            Ok(()) => Ok(println!("{}", p.program.string(0))),
+            Err(()) => {
+                println!("{}", p.error);
+                Err(format!("Failed to parse file: {}", trimmed_path))
+            }
+        }
     }
 }
