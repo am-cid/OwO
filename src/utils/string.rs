cf8506905144b8af6646cea164d500c12a1d8ffe
am-cid <mcidantonio@gmail.com>
2024-12-02 18:13:57 +0800

    styling: add colored and strikethrough text

--- src/utils/string.rs
+++ src/utils/string.rs
@@ -5,11 +5,120 @@ pub trait StringExt {
     fn pad_mid(&self, len: usize) -> String;
     fn fill_right(&self, len: usize) -> String;
     fn fill_left(&self, len: usize) -> String;
+    fn indent(&self, n: usize) -> String;
 
     // text formatting
     fn bold(&self) -> String;
     fn underline(&self) -> String;
     fn italic(&self) -> String;
+    fn strikethrough(&self) -> String;
+    fn red(&self) -> String;
+    fn green(&self) -> String;
+    fn yellow(&self) -> String;
+    fn blue(&self) -> String;
+    fn cyan(&self) -> String;
+    fn magenta(&self) -> String;
+    // \x1b[0m: reset
+}
+
+impl StringExt for str {
+    fn pad_right(&self, len: usize) -> String {
+        let width = match len < self.len() {
+            true => 0,
+            false => len - self.len(),
+        };
+        format!("{}{}", self, " ".repeat(width))
+    }
+    fn pad_left(&self, len: usize) -> String {
+        let width = match len < self.len() {
+            true => 0,
+            false => len - self.len(),
+        };
+        format!("{}{}", " ".repeat(width), self)
+    }
+    fn pad_mid(&self, len: usize) -> String {
+        let width = match len < self.len() {
+            true => 0,
+            false => len - self.len(),
+        };
+        format!(
+            "{}{}{}",
+            " ".repeat(width / 2),
+            self,
+            " ".repeat(width - width / 2)
+        )
+    }
+    fn fill_right(&self, len: usize) -> String {
+        format!("{}{}", self, " ".repeat(len))
+    }
+    fn fill_left(&self, len: usize) -> String {
+        format!("{}{}", " ".repeat(len), self)
+    }
+    fn indent(&self, n: usize) -> String {
+        self.fill_left(n * 4)
+    }
+    fn bold(&self) -> String {
+        let (leading, string) = match self.find(|c: char| !c.is_whitespace()) {
+            Some(idx) => self.split_at(idx),
+            None => ("", self),
+        };
+        let (string, trailing) = match string.rfind(|c: char| !c.is_whitespace()) {
+            Some(idx) => string.split_at(idx + 1),
+            None => (string, ""),
+        };
+        format!("{}\x1b[1m{}\x1b[0m{}", leading, string, trailing)
+    }
+    fn underline(&self) -> String {
+        let (leading, string) = match self.find(|c: char| !c.is_whitespace()) {
+            Some(idx) => self.split_at(idx),
+            None => ("", self),
+        };
+        let (string, trailing) = match string.rfind(|c: char| !c.is_whitespace()) {
+            Some(idx) => string.split_at(idx + 1),
+            None => (string, ""),
+        };
+        format!("{}\x1b[4m{}\x1b[0m{}", leading, string, trailing)
+    }
+    fn italic(&self) -> String {
+        let (leading, string) = match self.find(|c: char| !c.is_whitespace()) {
+            Some(idx) => self.split_at(idx),
+            None => ("", self),
+        };
+        let (string, trailing) = match string.rfind(|c: char| !c.is_whitespace()) {
+            Some(idx) => string.split_at(idx + 1),
+            None => (string, ""),
+        };
+        format!("{}\x1b[3m{}\x1b[0m{}", leading, string, trailing)
+    }
+    fn strikethrough(&self) -> String {
+        let (leading, string) = match self.find(|c: char| !c.is_whitespace()) {
+            Some(idx) => self.split_at(idx),
+            None => ("", self),
+        };
+        let (string, trailing) = match string.rfind(|c: char| !c.is_whitespace()) {
+            Some(idx) => string.split_at(idx + 1),
+            None => (string, ""),
+        };
+        format!("{}\x1b[9m{}\x1b[0m{}", leading, string, trailing)
+    }
+    fn red(&self) -> String {
+        format!("\x1b[31m{}\x1b[0m", self)
+    }
+    fn green(&self) -> String {
+        format!("\x1b[32m{}\x1b[0m", self)
+    }
+    fn yellow(&self) -> String {
+        format!("\x1b[33m{}\x1b[0m", self)
+    }
+    fn blue(&self) -> String {
+        format!("\x1b[94m{}\x1b[0m", self)
+    }
+    fn magenta(&self) -> String {
+        format!("\x1b[35m{}\x1b[0m", self)
+    }
+    fn cyan(&self) -> String {
+        format!("\x1b[36m{}\x1b[0m", self)
+    }
 }
 
 impl StringExt for String {
@@ -18,32 +127,35 @@ impl StringExt for String {
             true => 0,
             false => len - self.len(),
         };
-        return format!("{}{}", self, " ".repeat(width));
+        format!("{}{}", self, " ".repeat(width))
     }
     fn pad_left(&self, len: usize) -> String {
         let width = match len < self.len() {
             true => 0,
             false => len - self.len(),
         };
-        return format!("{}{}", " ".repeat(width), self);
+        format!("{}{}", " ".repeat(width), self)
     }
     fn pad_mid(&self, len: usize) -> String {
         let width = match len < self.len() {
             true => 0,
             false => len - self.len(),
         };
-        return format!(
+        format!(
             "{}{}{}",
             " ".repeat(width / 2),
             self,
             " ".repeat(width - width / 2)
-        );
+        )
     }
     fn fill_right(&self, len: usize) -> String {
-        return format!("{}{}", self, " ".repeat(len));
+        format!("{}{}", self, " ".repeat(len))
     }
     fn fill_left(&self, len: usize) -> String {
-        return format!("{}{}", " ".repeat(len), self);
+        format!("{}{}", " ".repeat(len), self)
+    }
+    fn indent(&self, n: usize) -> String {
+        self.fill_left(n * 4)
     }
     fn bold(&self) -> String {
         let (leading, string) = match self.find(|c: char| !c.is_whitespace()) {
@@ -77,5 +189,34 @@ impl StringExt for String {
             None => (string, ""),
         };
         format!("{}\x1b[3m{}\x1b[0m{}", leading, string, trailing)
+    }
+    fn strikethrough(&self) -> String {
+        let (leading, string) = match self.find(|c: char| !c.is_whitespace()) {
+            Some(idx) => self.split_at(idx),
+            None => ("", self.as_str()),
+        };
+        let (string, trailing) = match string.rfind(|c: char| !c.is_whitespace()) {
+            Some(idx) => string.split_at(idx + 1),
+            None => (string, ""),
+        };
+        format!("{}\x1b[9m{}\x1b[0m{}", leading, string, trailing)
+    }
+    fn red(&self) -> String {
+        format!("\x1b[31m{}\x1b[0m", self)
+    }
+    fn green(&self) -> String {
+        format!("\x1b[32m{}\x1b[0m", self)
+    }
+    fn yellow(&self) -> String {
+        format!("\x1b[33m{}\x1b[0m", self)
+    }
+    fn blue(&self) -> String {
+        format!("\x1b[94m{}\x1b[0m", self)
+    }
+    fn magenta(&self) -> String {
+        format!("\x1b[35m{}\x1b[0m", self)
+    }
+    fn cyan(&self) -> String {
+        format!("\x1b[36m{}\x1b[0m", self)
     }
 }
