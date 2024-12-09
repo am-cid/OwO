911a283d4096a6f7c449603dbfd977ea62dfc51a
am-cid <mcidantonio@gmail.com>
2024-12-09 22:32:39 +0800

    feat(cli): Command::validate() should also assign validated vals to fields, hence the mut specifier
    Command::new() to enforce args and flags fields if needed, but also
    allow a command to declare other fields where validate() can fill it up
    
    rename Command::parse() to Command::validate()
    
    rename tokenize() to parse_args()

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -3,31 +3,45 @@ use crate::cli::{
     version::Version,
 };
 
+/// owo cli command
 pub trait Command {
+    /// This should initialize other fields a command implementor have
+    fn new(arg: String, flags: Option<Vec<String>>) -> Self
+    where
+        Self: Sized;
+    /// Prints a short/verbose message depending on the verbose arg
     fn help_msg(verbose: bool)
     where
         Self: Sized;
-    fn parse(&self) -> Result<(), String>;
+    /// Should validate args and flags, then assign validated values to other fields (if applicable)
+    fn validate(&mut self) -> Result<(), String>;
+    /// Runs the functionality associated to the command
     fn exec(&self) -> Result<(), String>;
 }
-// Put commands here
+/// Tries to convert the passed in args to a [Command] implementor based on the name parameter.
+/// Does not do any sort of validation
 pub fn to_command(
     name: String,
     arg: String,
     flags: Option<Vec<String>>,
 ) -> Result<Box<dyn Command>, String> {
     match name.as_str() {
-        "help" | "" => Ok(Box::new(Help { arg, flags })),
-        "version" => Ok(Box::new(Version { arg, flags })),
-        "lex" => Ok(Box::new(Lex { arg, flags })),
-        "parse" => Ok(Box::new(Parse { arg, flags })),
-        "analyze" => Ok(Box::new(Analyze { arg, flags })),
-        "compile" => Ok(Box::new(Compile { arg, flags })),
-        "run" => Ok(Box::new(Run { arg, flags })),
+        "help" | "" => Ok(Box::new(Help::new(arg, flags))),
+        "version" => Ok(Box::new(Version::new(arg, flags))),
+        "lex" => Ok(Box::new(Lex::new(arg, flags))),
+        "parse" => Ok(Box::new(Parse::new(arg, flags))),
+        "analyze" => Ok(Box::new(Analyze::new(arg, flags))),
+        "compile" => Ok(Box::new(Compile::new(arg, flags))),
+        "run" => Ok(Box::new(Run::new(arg, flags))),
         _ => Err(format!("Unknown command {}", name)),
     }
 }
-pub fn tokenize(args: Vec<String>) -> Result<Box<dyn Command>, String> {
+/// collects the os args into easily parsable parts:
+/// - the command
+/// - the argument
+/// - flags
+/// in that order. This means that flags cannot come before the argument
+pub fn parse_args(args: Vec<String>) -> Result<Box<dyn Command>, String> {
     let mut args_iter = args.into_iter();
     let command = args_iter.next().unwrap_or_default();
     let arg = args_iter.next().unwrap_or_default();
