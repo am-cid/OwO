0c9beb1ea899e13b44f1ee127c44913e4f6547f6
am-cid <mcidantonio@gmail.com>
2024-12-09 22:32:39 +0800

    feat(cli): Command::validate() should also assign validated vals to fields, hence the mut specifier
    Command::new() to enforce args and flags fields if needed, but also
    allow a command to declare other fields where validate() can fill it up
    
    rename Command::parse() to Command::validate()
    
    rename tokenize() to parse_args()

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,8 @@ mod tests;
 mod utils;
 
 fn main() {
-    let os_args: Vec<String> = std::env::args().skip(1).collect();
-    cli::commands::tokenize(os_args)
-        .and_then(|cmd| cmd.parse().map(move |_| cmd))
+    cli::commands::parse_args(std::env::args().skip(1).collect())
+        .and_then(|mut cmd| cmd.validate().map(move |_| cmd))
         .and_then(|cmd| cmd.exec())
-        .unwrap_or_else(|e| println!("{}", e));
+        .unwrap_or_else(|e| println!("{e}"));
 }
